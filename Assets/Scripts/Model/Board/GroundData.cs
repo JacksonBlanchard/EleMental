cb9787f32e2744cdd3b85c74380d0fd05f73d99e
Jackson Blanchard <JacksonBlanchard@github.com>
2022-01-16 20:07:27 -0500

    New ScriptableObjects for data

---
+++ Assets/Scripts/Model/Board/GroundData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundData : ScriptableObject
+{
+    public int width;
+    public int length;
+    public GroundEnum[] ground;
+}
